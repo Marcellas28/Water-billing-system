1fba334c0758fb31ef1014aa848ea21a75228a67
Marcellas Indeje <danwits28@gmail.com>
2025-05-15 10:37:46 +0300

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,269 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        Label9 = New Label()
+        Label10 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        TextBox5 = New TextBox()
+        Button1 = New Button()
+        Label11 = New Label()
+        TextBox6 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Showcard Gothic", 18F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label1.ForeColor = SystemColors.HotTrack
+        Label1.Location = New Point(356, 9)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(430, 44)
+        Label1.TabIndex = 0
+        Label1.Text = "WATER BILLING SYSTEM"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Algerian", 10F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label2.Location = New Point(59, 95)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(178, 22)
+        Label2.TabIndex = 1
+        Label2.Text = "Customer Name :"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(59, 145)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(0, 25)
+        Label3.TabIndex = 2
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(59, 253)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(0, 25)
+        Label4.TabIndex = 3
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(133, 295)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(0, 25)
+        Label5.TabIndex = 4
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(59, 330)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(0, 25)
+        Label6.TabIndex = 5
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Font = New Font("Algerian", 10F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label7.Location = New Point(115, 197)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(122, 22)
+        Label7.TabIndex = 6
+        Label7.Text = "Residence :"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Font = New Font("Algerian", 10F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label8.Location = New Point(122, 133)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(115, 22)
+        Label8.TabIndex = 7
+        Label8.Text = "Phone No. :"
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.BackColor = SystemColors.InactiveCaption
+        Label9.Font = New Font("AcmeFont", 10F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label9.Location = New Point(12, 306)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(237, 24)
+        Label9.TabIndex = 8
+        Label9.Text = "Previous Unit Reading :"
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.BackColor = SystemColors.InactiveCaption
+        Label10.Font = New Font("AcmeFont", 10F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label10.Location = New Point(27, 355)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(222, 24)
+        Label10.TabIndex = 9
+        Label10.Text = "Current Unit Reading :"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.BackColor = SystemColors.InactiveCaption
+        TextBox1.Location = New Point(253, 86)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(229, 31)
+        TextBox1.TabIndex = 10
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.BackColor = SystemColors.InactiveCaption
+        TextBox2.Location = New Point(253, 188)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(150, 31)
+        TextBox2.TabIndex = 11
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.BackColor = SystemColors.MenuHighlight
+        TextBox3.Location = New Point(274, 352)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(82, 31)
+        TextBox3.TabIndex = 12
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.BackColor = SystemColors.MenuHighlight
+        TextBox4.Location = New Point(276, 306)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(80, 31)
+        TextBox4.TabIndex = 13
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.BackColor = SystemColors.InactiveCaption
+        TextBox5.Location = New Point(253, 128)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(188, 31)
+        TextBox5.TabIndex = 14
+        ' 
+        ' Button1
+        ' 
+        Button1.BackgroundImage = CType(resources.GetObject("Button1.BackgroundImage"), Image)
+        Button1.BackgroundImageLayout = ImageLayout.Zoom
+        Button1.Font = New Font("Algerian", 11F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button1.ForeColor = SystemColors.HotTrack
+        Button1.Location = New Point(411, 307)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(149, 76)
+        Button1.TabIndex = 15
+        Button1.Text = "Get "
+        Button1.TextAlign = ContentAlignment.MiddleLeft
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label11
+        ' 
+        Label11.AutoSize = True
+        Label11.BackColor = SystemColors.HotTrack
+        Label11.BorderStyle = BorderStyle.Fixed3D
+        Label11.FlatStyle = FlatStyle.Flat
+        Label11.Font = New Font("Times New Roman", 16F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label11.Location = New Point(938, 401)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(27, 38)
+        Label11.TabIndex = 16
+        Label11.Text = "-"
+        ' 
+        ' TextBox6
+        ' 
+        TextBox6.Font = New Font("Magneto", 10F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        TextBox6.Location = New Point(1018, 710)
+        TextBox6.Name = "TextBox6"
+        TextBox6.Size = New Size(193, 32)
+        TextBox6.TabIndex = 17
+        TextBox6.Text = "Marcellas Did It"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), Image)
+        BackgroundImageLayout = ImageLayout.Stretch
+        ClientSize = New Size(1223, 740)
+        Controls.Add(TextBox6)
+        Controls.Add(Button1)
+        Controls.Add(TextBox5)
+        Controls.Add(TextBox4)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label10)
+        Controls.Add(Label9)
+        Controls.Add(Label8)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(Label11)
+        ForeColor = SystemColors.Desktop
+        FormBorderStyle = FormBorderStyle.Fixed3D
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label11 As Label
+    Friend WithEvents TextBox6 As TextBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,61 @@
+﻿Public Class Form1
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub TextBox5_TextChanged(sender As Object, e As EventArgs) Handles TextBox5.TextChanged
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+    End Sub
+
+    Private Sub TextBox4_TextChanged(sender As Object, e As EventArgs) Handles TextBox4.TextChanged
+
+    End Sub
+
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
+
+    End Sub
+
+    Private Sub Label11_Click(sender As Object, e As EventArgs) Handles Label11.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Customer_Name As String = TextBox1.Text
+        Dim Phone_No As Integer = TextBox5.Text
+        Dim Residence As String = TextBox2.Text
+        Dim Previous As Double = TextBox4.Text
+        Dim Current As Double = TextBox3.Text
+
+        Dim reading As Double = Current - Previous
+        Dim Bill As Double
+
+        Select Case reading
+            Case 0 To 4
+                Bill = reading * 10 + 500
+            Case 5 To 6
+                Bill = reading * 20 + 500
+            Case 1 To 9
+                Bill = reading * 15 + 500
+            Case Is > 10
+                Bill = reading * 10 + 500
+        End Select
+
+        Label11.Text = "========Water Billing system=========" & vbCrLf &
+            "Customer Name :  " & Customer_Name & vbCrLf &
+            "Phone Number :  " & Phone_No & vbCrLf &
+            "Residence : " & Residence & vbCrLf &
+            "Previous Water Reading : " & Previous & vbCrLf &
+            "Current Water Reading : " & Current & vbCrLf &
+            "Water per Unit Consumed : " & reading & vbCrLf &
+            "Final Water Bill: " & Bill & vbCrLf &
+            "=================================="
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
